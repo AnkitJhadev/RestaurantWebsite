1379a194a5f16582a6c0f4407091c1de5ef1905b
AnkitJhadev <jha80250@gmail.com>
2024-02-11 09:34:49 +0530

    Added Header to file

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,27 @@
+import './Header.css'
+
+const Header = () => {
+
+    return (
+      <>
+      
+      <header className='header_style'>
+      <h3>React Meals</h3>
+      <h3 className='your_cart'>Your Cart<button>{0}</button></h3>
+
+
+
+      </header>
+      
+      
+      
+      
+      </>
+
+
+    );
+
+
+}
+
+export default Header;
